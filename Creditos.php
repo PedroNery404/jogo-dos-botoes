64b09df12539cce4cfa6f98ff5931ec23885ded7
PedroNery404 <naosesabe0@gmail.com>
2020-07-24 17:04:56 -0300

    Correçao de texto

--- Creditos.php
+++ Creditos.php
@@ -19,17 +19,34 @@
 			font-size: 30px;
 			font-family: 'Pangolin', cursive;
 		}
+		button{
+			background-color: #4DE8AA;
+			width: 400px;
+			height: 50px;
+			font-size: 25px;
+			margin:40px;
+			color: darkblue;
+			border-radius: 50px;
+			box-shadow: 10px 10px 10px rgba(0,0,0,0.5);
+			font-style: italic;
+			font-family: 'Trade Winds', cursive;
+		}
+		button:hover{
+			background-color: gray;
+			cursor: pointer;
+		}
 	</style>
 </head>
 <body>
 	<center>
 		<p class="Titulo">Agradecimentos Especiais</p>
 		<div>
-			<p>Esse jogo foi pensado e produzido por Pedro Nery com o intuito de testar suas habilidades e tentar fazer um jogo com o desafio da torre do fogo do jogo Forager.</p>
-			<p>Para nao apenas copiar e colar o jogo tentei colocar alguns extras para melhorar a ideia e deixar o jogo mais interessante e desafiador.</p>
-			<p>Esse jogo não é a versão final,é apenas um prototipo,ou seja,atualizações serão feitas.</p>
-			<p>Obrigado a todos aqueles que jogaram e gostaram do jogo que tentei fazer.</p>
+			<p>Esse jogo foi pensado e produzido por Pedro Nery com o intuito de testar suas habilidades e tentar fazer um jogo com o desafio da torre do fogo do game Forager.</p>
+			<p>Para nao apenas copiar e colar o jogo tentei colocar alguns extras para melhorar a ideia e deixar a brincadeira mais interessante e desafiadora.</p>
+			<p>Essa versão é apenas um prototipo,ou seja,atualizações serão feitas.</p>
+			<p>Obrigado a todos aqueles que jogaram e gostaram do desafio que tentei fazer.</p>
 		</div>
+		<a href="index.php"><button>Voltar</button></a>
 	</center>
 </body>
 </html>
