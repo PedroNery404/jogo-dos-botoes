34db77bdc108a40b5b9888c5086a5d5341f341a9
PedroNery404 <naosesabe0@gmail.com>
2020-07-30 23:09:41 -0300

    Finalização do Jogo

--- Jogar.php
+++ Jogar.php
@@ -2,11 +2,13 @@
 <html>
 <head>
 	<link href="https://fonts.googleapis.com/css?family=Trade+Winds&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Pangolin&display=swap" rel="stylesheet">
 	<meta charset="utf-8">
 	<title>Jogo Dos Botões - Menu</title>
 	<style type="text/css">
 		body{
 			background-color: lightblue;
+			font-family: 'Pangolin', cursive;	
 		}
 		button{
 			background-color: #4DE8AA;
@@ -19,6 +21,7 @@
 			box-shadow: 10px 10px 10px rgba(0,0,0,0.5);
 			font-style: italic;
 			font-family: 'Trade Winds', cursive;
+			outline: none;
 		}
 		a{
 			text-decoration: none;
@@ -27,14 +30,34 @@
 			background-color: gray;
 			cursor: pointer;
 		}
+		.hidden{
+			display: none;
+		}
+		input{
+			height: 20px;
+			padding: 8px;
+			outline: none;
+			border-radius:50px; 
+			display: block;
+		}
 	</style>
+	<script type="text/javascript">
+		function Treino(x) {
+			if (document.getElementById(x).style.display=='block') {
+				document.getElementById(x).style.display='none';
+			} 
+			else {
+				document.getElementById(x).style.display='block';
+			}
+		}
+	</script>
 </head>
 <body>
 	<center>
 		<br><br>
 		<a href="Niveis.php"><button>Modo de Niveis</button></a>
 		<br>
-		<a href="Jogo.php?Modo=2&Valor=2"><button>Modo Infinito</button></a>
+		<a href="Jogo.php?Modo=2&Valor=2&Botoes=--"><button>Modo Infinito</button></a>
 		<br>
 		<?php
 			$sequencia=[];
@@ -44,9 +67,18 @@
 				array_push($sequencia, $numero);
 			}
 			$botoes1=implode('-', $sequencia);
-			echo "<a href='Jogo.php?Modo=3&Valor=2&Botoes=$botoes1'><button>Modo Aleatorio</button></a><br>";	
-			echo "<a href='Jogo.php?Modo=4&Valor=2&Botoes=$botoes1'><button>Modo Corrida Infinita</button></a><br>";
+			echo "<a href='Jogo.php?Modo=3&Valor=2&Botoes=$botoes1--'><button>Modo Aleatorio</button></a>";	
 		?>
+		<br>
+		<button onclick="Treino('EscolherNivel')" class="ModoTreino">Modo Treino</button>
+		<div class="hidden" id="EscolherNivel">
+			<form action="Jogo.php?" method="GET">
+				<label style="font-size: 20px;">Escolha o Nível que deseja Começar<br><br></label>
+				<input type="number" name="Valor" min="2">
+				<button type="submit" style="width: 100px;height: 40px;font-size: 20px;">OK</button>
+			</form>
+		</div>
+		<br>
 		<a href="index.php"><button>Voltar</button></a>
 	</center>
 </body>
--- Recordes.php
+++ Recordes.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">	
 	<link href="https://fonts.googleapis.com/css?family=Trade+Winds&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Pangolin&display=swap" rel="stylesheet">
 	<meta charset="utf-8">
 	<title>Jogo Dos Botões - Menu</title>
 	<style type="text/css">
@@ -19,28 +21,62 @@
 			box-shadow: 10px 10px 10px rgba(0,0,0,0.5);
 			font-style: italic;
 			font-family: 'Trade Winds', cursive;
+			outline: none;
 		}
 		a{
 			text-decoration: none;
+		}
+		div{
+			width: 65%;
+			font-size: 30px;
+			font-family: 'Pangolin', cursive;
 		}
 		button:hover{
 			background-color: gray;
 			cursor: pointer;
 		}
+		.Titulo{
+			font-size: 40px;
+			color: darkblue;
+			font-family: 'Trade Winds', cursive
+		}
 	</style>
 </head>
 <body>
 	<center>
-		<br><br>
-		<a href="Jogar.php"><button>Jogar</button></a>
-		<br>
-		<a href="Instruçoes.php"><button>Instruções</button></a>
-		<br>
-		<a href="Recordes.php"><button>Recordes</button></a>
-		<br>
-		<a href="Configuraçoes.php"><button>Configurações</button></a>
-		<br>
-		<a href="Creditos.php"><button>Créditos</button></a>
+		<p class="Titulo">Recordes</p>
+		<div>
+		<?php
+			$con= mysqli_connect("localhost","id13179308_root","abcdeFGHIJ-1234","id13179308_jogodosbotoes");
+			$nick=filter_input(INPUT_GET,'Nick', FILTER_SANITIZE_NUMBER_INT);
+			$nivel=filter_input(INPUT_GET,'Nivel', FILTER_SANITIZE_NUMBER_INT);
+			if(isset($_GET['Nick'])){
+  				$nick=addslashes($_GET["Nick"]);
+				$Data=[date('Y'),date('n'),date('d')];
+				$Hora=[date('H'),date('i'),date('s')];
+				$Data2=implode('-', $Data);
+				$Hora2=implode(':', $Hora);
+				$sql1=mysqli_query($con,"INSERT INTO recordes(Usuario,MaiorNivel,Data) VALUES ('$nick','$nivel','$Data2 $Hora2')") or print mysql_error();
+			}
+			$sql1=mysqli_query($con,"SELECT * FROM recordes") or print mysql_error();
+			$row=mysqli_num_rows($sql1);
+			if ($row>0) {
+        		while ($linha=mysqli_fetch_array($sql1)) {
+            		$Codigo=$linha['Cod_Recorde'];
+           			$Usuario=$linha['Usuario'];
+            		$Nivel=$linha['MaiorNivel'];
+        		}
+    		}
+    		$comando="SELECT * FROM recordes ORDER BY MaiorNivel desc LIMIT 10";
+    		$sql2=mysqli_query($con,$comando) or print mysql_error();
+    		while($row_usuario = mysqli_fetch_assoc($sql2)){
+            	$Cod_Recorde=$row_usuario['Cod_Recorde'];
+            	echo "<a style='text-decoration: none;font-size: 25px;'>Usuario: " . $row_usuario['Usuario'] . "<br>";
+            	echo "Maior Nivel: " . $row_usuario['MaiorNivel'] ."<br><hr>";
+        }
+		?>
+		</div>
+		<a href="index.php"><button>Voltar</button></a>
 	</center>
 </body>
 </html>
